f9987839ded92ee2dede4e42ae81f153b2648a30
Martin Vejnár <vejnar.martin@gmail.com>
2025-03-20 14:53:02 +0100

    xxx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,132 @@
 'use client'
 
-import { Db, parse } from "./parser"
+import { parse, parse_worldgen } from "./parser"
 import { DragEvent, useState } from 'react'
+import { gen_traits, geysers } from "./world_traits"
+import { KRand } from "./krand"
+
+function defaultGridProps(): GridProps {
+    return { seed: "", width: 0, height: 0, cells: [] }
+}
 
 async function parseSaveFile(data: Blob): Promise<GridProps> {
-    const saveFile = await parse(await data.arrayBuffer())
+    //const saveFile = await parse(await data.arrayBuffer())
 
     const cells: Cell[] = []
 
-    let id = 0
-    for (const obj of saveFile.objs) {
-        id += 1
-        if ('WorldContainer' in obj.props) {
-            cells.push({
-                id,
-                type: obj.tag,
-                elemId: 0,
-                position: obj.props['WorldContainer']['worldOffset'],
-                size: obj.props['WorldContainer']['worldSize'],
-                text: obj.props['WorldContainer']['worldName'],
-            })
-        }
-        // if (obj.tag == 'SaveGame') {
-        //     for (const si of obj.props['WorldGenSpawner']['spawnInfos']) {
-        //         if (
-        //         cells.push({
-        //             type: 'spawner',
-        //             position: {
-        //                 x: si.location_x,
-        //                 y: si.location_y,
-        //             },
-        //             elemId: si.element,
-        //         })
-        //     }
-        // }
-        // if ('PrimaryElement' in obj.props) {
-        //     const position = obj.position
-        //     const elemId = (obj.props['PrimaryElement'] as Db)['ElementID'] as number
+    const worldgen = await parse_worldgen(await data.arrayBuffer()) as any
+    for (const world of worldgen.worlds) {
+        const offset = world.data.world.offset
+        const sd = world.data.gameSpawnData
 
-        //     cells.push({ position, elemId, type: obj.tag })
-        // }
+        cells.push({
+            size: { x: world.data.world.size.x, y: world.data.world.size.y },
+            position: { x: offset.x, y: offset.y },
+            color: "gray",
+            text: world.name,
+        })
+
+        for (const item of sd.buildings) {
+            if (item.id.indexOf("quarters") >= 0) {
+                cells.push({
+                    size: { x: 10, y: 10 },
+                    position: { x: offset.x + item.location_x, y: offset.y + item.location_y },
+                    color: "green",
+                    text: item.id,
+                })
+            }
+        }
+
+        for (const item of sd.otherEntities) {
+            if (item.id.indexOf('Geyser') >= 0) {
+                if (item.id == 'GeyserGeneric') {
+                    const seed = world.data.globalWorldSeed + Math.floor(item.location_x) + Math.floor(item.location_y) + 7
+                    const r = new KRand(seed)
+                    const idx = r.int(0, geysers.length)
+                    item.id = `GeyserGeneric_${geysers[idx]}`
+                }
+
+                cells.push({
+                    //elemId: 1,
+                    //id: 1,
+                    size: { x: 4, y: 4 },
+                    position: { x: offset.x + item.location_x, y: offset.y + item.location_y },
+                    //type: "geyser",
+                    color: 'red',
+                    text: (item.id as string).slice(14),
+                })
+            }
+            // } else if (item.id.indexOf('Seed') < 0 && item.id.indexOf('Plant') < 0 && item.id.indexOf('Flower') < 0 && item.id.indexOf('Grass') < 0) {
+            //     cells.push({
+            //         elemId: 1,
+            //         id: 1,
+            //         size: { x: 1, y: 1 },
+            //         position: { x: offset.x + item.location_x, y: offset.y + item.location_y },
+            //         type: "geyser",
+            //         text: item.id,
+            //     })
+            // }
+        }
     }
 
     return {
-        seed: saveFile.settings,
-        width: saveFile.root['WidthInCells'] as number,
-        height: saveFile.root['HeightInCells'] as number,
-        cells
+        seed: worldgen.worlds[0].data.globalWorldSeed,
+        width: worldgen.size.x,
+        height: worldgen.size.y,
+        cells: cells
     }
+
+    // const cells: Cell[] = []
+
+    // let id = 0
+    // for (const obj of saveFile.objs) {
+    //     id += 1
+    //     if ('WorldContainer' in obj.props) {
+    //         cells.push({
+    //             id,
+    //             type: obj.tag,
+    //             elemId: 0,
+    //             position: obj.props['WorldContainer']['worldOffset'],
+    //             size: obj.props['WorldContainer']['worldSize'],
+    //             text: obj.props['WorldContainer']['worldName'],
+    //         })
+    //     }
+    //     // if (obj.tag == 'SaveGame') {
+    //     //     for (const si of obj.props['WorldGenSpawner']['spawnInfos']) {
+    //     //         if (
+    //     //         cells.push({
+    //     //             type: 'spawner',
+    //     //             position: {
+    //     //                 x: si.location_x,
+    //     //                 y: si.location_y,
+    //     //             },
+    //     //             elemId: si.element,
+    //     //         })
+    //     //     }
+    //     // }
+    //     // if ('PrimaryElement' in obj.props) {
+    //     //     const position = obj.position
+    //     //     const elemId = (obj.props['PrimaryElement'] as Db)['ElementID'] as number
+
+    //     //     cells.push({ position, elemId, type: obj.tag })
+    //     // }
+    // }
+
+    // return {
+    //     seed: saveFile.settings,
+    //     width: saveFile.root['WidthInCells'] as number,
+    //     height: saveFile.root['HeightInCells'] as number,
+    //     cells
+    // }
 }
 
 interface Cell {
-    id: number
+    //id: number
     position: { x: number, y: number }
     size: { x: number, y: number }
-    type: string
-    elemId: number
+    //type: string
+    //elemId: number
+    color: string
     text?: string
 }
 
@@ -69,27 +140,27 @@ interface GridProps {
 const cellSize = 5
 
 function Grid(props: GridProps) {
-    const divs = props.cells.map(cell => {
-        return <div key={cell.id} style={{
+    const divs = props.cells.map((cell, index) => {
+        return <div key={index} style={{
             position: "absolute",
             left: cell.position.x*cellSize,
             top: (props.height - cell.position.y - cell.size.y)*cellSize,
             width: cell.size.x * cellSize,
             height: cell.size.y * cellSize,
-            backgroundColor: 'red',
-            fontSize: 48,
+            backgroundColor: cell.color,
+            textAlign: 'center',
+            //fontSize: 48,
         }}>{cell.text}</div>
     });
 
     return <div style={{
         width: props.width*cellSize,
         height: props.height*cellSize,
-        backgroundColor: "gray"
     }}>{divs}</div>
 }
 
 export default function Home() {
-    const [grid, setGrid] = useState<GridProps>(() => ({ seed: "", width: 0, height: 0, cells: [] }))
+    const [grid, setGrid] = useState<GridProps>(defaultGridProps)
 
     const handleDragOver = (e: DragEvent<HTMLDivElement>) => {
         e.preventDefault()
@@ -110,8 +181,53 @@ export default function Home() {
         }
     }
 
+    const [seeds, setSeeds] = useState<string[]>(() => [])
+
+    const generateSeed = () => {
+        const isValidSeed = (traits: string[]) => {
+            for (const trait of ['GeoActive', 'MetalRich', 'expansion1::IrregularOil']) {
+                if (!traits.includes(trait))
+                    return false
+            }
+
+            for (const trait of ['BouldersLarge', 'BouldersMedium', 'BouldersMixed', 'BouldersSmall', 'GlaciersLarge', 'SlimeSplats', 'GeoDormant']) {
+                if (traits.includes(trait))
+                    return false
+            }
+
+            return true
+        }
+
+        while (true) {
+            const seed = Math.trunc(Math.random()*2000000000)
+            const traits = gen_traits(seed)
+
+            if (isValidSeed(traits)) {
+                setSeeds([...seeds, `V-SFRZ-C-${seed}-0-D3-UE2 ${traits}`])
+                break
+            }
+        }
+    }
+
+    const seedItems = []
+    for (const sd of seeds) {
+        seedItems.push(<li>{sd}</li>)
+    }
+
+    const xxx = () => {
+        for (let i = -100; i < 100; i++) {
+            const r = new KRand(444812768 + 38 + 225 + i)
+            const idx = r.int(0, geysers.length)
+            if (geysers[idx] == 'molten_aluminum')
+                console.log(i)
+        }
+    }
+
     return (
-        <div className="wide" onDrop={handleDrop} onDragOver={handleDragOver}>
+        <div onDrop={handleDrop} onDragOver={handleDragOver}>
+            <p><button onClick={xxx}>xxx</button></p>
+            <p><button onClick={generateSeed}>Generate seed</button></p>
+            <ul>{seedItems}</ul>
             <Grid seed={grid.seed} width={grid.width} height={grid.height} cells={grid.cells}></Grid>
         </div>
     );
--- src/app/parser.ts
+++ src/app/parser.ts
@@ -1,70 +1,5 @@
 import { KleiReader } from './array_reader'
-
-enum SerializationTypeInfo {
-	UserDefined = 0,
-	SByte = 1,
-	Byte = 2,
-	Boolean = 3,
-	Int16 = 4,
-	UInt16 = 5,
-	Int32 = 6,
-	UInt32 = 7,
-	Int64 = 8,
-	UInt64 = 9,
-	Single = 10,
-	Double = 11,
-	String = 12,
-	Enumeration = 13,
-	Vector2I = 14,
-	Vector2 = 15,
-	Vector3 = 16,
-	Array = 17,
-	Pair = 18,
-	Dictionary = 19,
-	List = 20,
-	HashSet = 21,
-	Queue = 22,
-	Color = 23,
-	IS_GENERIC_TYPE = 128,
-	IS_VALUE_TYPE = 64,
-	VALUE_MASK = 63,
-}
-
-type SavType = {
-    kind:
-        'u8' | 'i8' | 'u16' | 'i16' | 'u32' | 'i32' | 'u64' | 'i64'
-        | 'f32' | 'f64'
-        | 'bool' | 'str' | 'v2i32' | 'v2f32' | 'v3f32' | 'color'
-} | {
-    kind: 'enum'
-    name: string
-} | {
-    kind: 'class' | 'struct'
-    name: string
-    args: SavType[]
-} | {
-    kind: 'array' | 'list' | 'set' | 'queue'
-    base_tp: SavType
-} | {
-    kind: 'dict'
-    key_tp: SavType
-    value_tp: SavType
-} | {
-    kind: 'pair'
-    first_tp: SavType
-    second_tp: SavType
-}
-
-interface SavField {
-    name: string
-    type: SavType
-}
-
-interface SavClass {
-    name: string
-    fields: SavField[]
-    props: SavField[]
-}
+import { Db, Deser, read_types } from './deser'
 
 interface Vec3 {
     x: number
@@ -77,12 +12,6 @@ interface Vec4 {
     y: number
     z: number
     w: number
-}
-
-type SavValue = null | boolean | number | string | ArrayBuffer | Set<SavValue> | Db | SavValue[]
-
-export interface Db {
-    [key: string]: SavValue
 }
 
 interface GameObject {
@@ -101,10 +30,15 @@ interface SaveFile {
     game_save_data: any
 }
 
-// export async function parse_worldgen(data: ArrayBuffer): Promise<SaveFile> {
-//     let rd = new KleiReader(data)
-//     const types = read_types(rd)
-// }
+export async function parse_worldgen(data: ArrayBuffer): Promise<Db> {
+    let rd = new KleiReader(data)
+    const types = read_types(rd)
+    const deser = new Deser(rd, types)
+
+    const cluster_layout_save = deser.deser_obj()
+
+    return cluster_layout_save
+}
 
 export async function parse(data: ArrayBuffer): Promise<SaveFile> {
     let rd = new KleiReader(data)
@@ -158,174 +92,15 @@ export async function parse(data: ArrayBuffer): Promise<SaveFile> {
 
     const world = rd.nullable_string()
 
-    const deser_cls = (cls: SavClass, size?: number) => {
-        const next_pos = size? rd.tell() + size: undefined
-
-        const obj: Db = {}
-        for (const field of cls.fields) {
-            obj[field.name] = deser_tp(field.type)
-        }
-        for (const prop of cls.props) {
-            obj[prop.name] = deser_tp(prop.type)
-        }
-
-        if (next_pos !== undefined && rd.tell() < next_pos) {
-            obj['.extra'] = next_pos - rd.tell()
-            rd.seek(next_pos)
-            //obj['.extra'] = rd.bytes(next_pos - pos)
-        }
-
-        return obj
-    }
-
-    const deser_tp = (tp: SavType, allow_value_types: boolean = false): SavValue => {
-        switch (tp.kind) {
-            case 'bool':
-                return rd.u8() == 1
-
-            case 'enum':
-            case "i32":
-                return rd.i32()
-
-            case "u32":
-                return rd.u32()
-
-            case "f32":
-                return rd.f32()
-
-            case "v2i32":
-                return rd.v2i32()
-            case "v2f32":
-                return rd.v2f32()
-            case "v3f32":
-                return rd.v3f32()
-
-            case 'str': {
-                return rd.nullable_string()
-            }
-
-            case 'dict': {
-                const size = rd.i32()
-                const len = rd.i32()
-
-                if (len < 0)
-                    return null
-
-                const values = []
-
-                const r: Db = {}
-
-                for (let i = 0; i != len; i++) {
-                    values.push(deser_tp(tp.value_tp))
-                }
-                for (let i = 0; i != len; i++) {
-                    const key = deser_tp(tp.key_tp)
-                    if (typeof key === 'string')
-                        r[key] = values[i]
-                }
-
-                return r
-            }
-
-            case 'set': {
-                const size = rd.i32()
-                const len = rd.i32()
-
-                if (len < 0) {
-                    return null
-                }
-
-                const r = new Set<any>()
-                for (let i = 0; i != len; i++) {
-                    r.add(deser_tp(tp.base_tp, true))
-                }
-                return r
-            }
-
-            case 'list':
-            case 'array': {
-                const size = rd.i32()
-                const len = rd.i32()
-
-                if (len < 0) {
-                    return null
-                }
-
-                switch (tp.base_tp.kind) {
-                    case 'u8': {
-                        return rd.bytes(len)
-                    }
-                    default: {
-                        const r = []
-                        for (let i = 0; i != len; i++) {
-                            r.push(deser_tp(tp.base_tp, true))
-                        }
-                        return r
-                    }
-                }
-            }
-
-            case 'class': {
-                const size = rd.i32()
-
-                if (size < 0)
-                    return null
-
-                const cls = types.get(tp.name)
-                if (cls === undefined) {
-                    rd.skip(size)
-                    return null // XXX
-                }
-
-                return deser_cls(cls, size)
-            }
-
-            case 'struct': {
-                let size
-                if (!allow_value_types) {
-                    size = rd.i32()
-                }
-
-                const cls = types.get(tp.name)
-                if (cls === undefined) {
-                    if (size === undefined)
-                        throw "XXX"
-                    rd.skip(size)
-                    return null // XXX
-                }
-
-                return deser_cls(cls, size)
-            }
-
-            case 'pair': {
-                const size = rd.i32()
-                const first = deser_tp(tp.first_tp)
-                const second = deser_tp(tp.second_tp)
-                return [first, second]
-            }
-
-            default: {
-                throw "XXX"
-            }
-        }
-    }
-
-    const deser_obj = (): Db => {
-        const tp_name = rd.string()
-        const cls = types.get(tp_name)
-        if (!cls)
-            throw "XXX"
-
-        return deser_cls(cls)
-    }
+    const deser = new Deser(rd, types)
 
     // const objs = []
     // while (pos != content.length) {
     //     const obj = deser_obj()
     //     objs.push(obj)
     // }
-    const save_file_root = deser_obj()
-    const settings = deser_obj()
+    const save_file_root = deser.deser_obj()
+    const settings = deser.deser_obj()
 
     const load_sim = () => {
         const len = rd.i32()
@@ -368,7 +143,7 @@ export async function parse(data: ArrayBuffer): Promise<SaveFile> {
                     if (cls === undefined) {
                         rd.skip(component_size)
                     } else {
-                        props[component_name] = deser_cls(cls, component_size)
+                        props[component_name] = deser.deser_cls(cls, component_size)
                     }
                 }
 
@@ -381,7 +156,7 @@ export async function parse(data: ArrayBuffer): Promise<SaveFile> {
 
     const objs = load_ksav()
 
-    const game_save_data = deser_obj()
+    const game_save_data = deser.deser_obj()
 
     return {
         header,
@@ -390,154 +165,4 @@ export async function parse(data: ArrayBuffer): Promise<SaveFile> {
         objs,
         game_save_data,
     }
-}
-
-function read_types(rd: KleiReader) {
-    const read_type = (): SavType => {
-        const flags = rd.u8()
-
-        const is_value_type = (flags & SerializationTypeInfo.IS_VALUE_TYPE)
-
-        const read_args = (n?: number): SavType[] => {
-            const args: SavType[] = []
-            if (flags & SerializationTypeInfo.IS_GENERIC_TYPE) {
-                const arg_count = rd.u8()
-                if (n !== undefined && arg_count != n) {
-                    throw "XXX"
-                }
-
-                for (let i = 0; i != arg_count; i++) {
-                    args.push(read_type())
-                }
-            }
-            return args
-        }
-
-        const check_no_args = () => {
-            if (flags & SerializationTypeInfo.IS_GENERIC_TYPE) {
-                const arg_count = rd.u8()
-                if (arg_count) {
-                    throw "XXX"
-                }
-            }
-        }
-
-        switch (flags & SerializationTypeInfo.VALUE_MASK) {
-            case SerializationTypeInfo.Pair: {
-                const [first_tp, second_tp] = read_args(2)
-                return { kind: "pair", first_tp, second_tp }
-            }
-            case SerializationTypeInfo.Dictionary: {
-                const [key_tp, value_tp] = read_args(2)
-                return { kind: "dict", key_tp, value_tp }
-            }
-            case SerializationTypeInfo.List: {
-                const [base_tp] = read_args(1)
-                return { kind: "list", base_tp }
-            }
-            case SerializationTypeInfo.HashSet: {
-                const [base_tp] = read_args(1)
-                return { kind: "set", base_tp }
-            }
-            case SerializationTypeInfo.Queue: {
-                const [base_tp] = read_args(1)
-                return { kind: "queue", base_tp }
-            }
-            case SerializationTypeInfo.Enumeration: {
-                const name = rd.string()
-                check_no_args()
-                return { kind: "enum", name }
-            }
-            case SerializationTypeInfo.UserDefined: {
-                const name = rd.string()
-                const args = read_args()
-                return { kind: is_value_type? "struct": "class", name, args }
-            }
-            case SerializationTypeInfo.Array: {
-                const base_tp = read_type()
-                check_no_args()
-                return { kind: "array", base_tp }
-            }
-
-            case SerializationTypeInfo.SByte:
-                check_no_args()
-                return { kind: "i8" }
-            case SerializationTypeInfo.Byte:
-                check_no_args()
-                return { kind: "u8" }
-            case SerializationTypeInfo.Boolean:
-                check_no_args()
-                return { kind: "bool" }
-            case SerializationTypeInfo.Int16:
-                check_no_args()
-                return { kind: "i16" }
-            case SerializationTypeInfo.UInt16:
-                check_no_args()
-                return { kind: "u16" }
-            case SerializationTypeInfo.Int32:
-                check_no_args()
-                return { kind: "i32" }
-            case SerializationTypeInfo.UInt32:
-                check_no_args()
-                return { kind: "u32" }
-            case SerializationTypeInfo.Int64:
-                check_no_args()
-                return { kind: "i64" }
-            case SerializationTypeInfo.UInt64:
-                check_no_args()
-                return { kind: "u64" }
-            case SerializationTypeInfo.Single:
-                check_no_args()
-                return { kind: "f32" }
-            case SerializationTypeInfo.Double:
-                check_no_args()
-                return { kind: "f64" }
-            case SerializationTypeInfo.String:
-                check_no_args()
-                return { kind: "str" }
-            case SerializationTypeInfo.Vector2I:
-                check_no_args()
-                return { kind: "v2i32" }
-            case SerializationTypeInfo.Vector2:
-                check_no_args()
-                return { kind: "v2f32" }
-            case SerializationTypeInfo.Vector3:
-                check_no_args()
-                return { kind: "v3f32" }
-            case SerializationTypeInfo.Color:
-                check_no_args()
-                return { kind: "color" }
-            default:
-                throw "XXX"
-        }
-    }
-
-    const types = new Map<string, SavClass>()
-
-    const type_count = rd.i32()
-    for (let i = 0; i != type_count; i++) {
-        const name = rd.string()
-
-        const field_count = rd.i32()
-        const property_count = rd.i32()
-
-        const fields: SavField[] = []
-        const props: SavField[] = []
-
-        for (let j = 0; j != field_count; j++) {
-            const name = rd.string()
-            const type = read_type()
-            fields.push({ name, type })
-        }
-
-        for (let j = 0; j != property_count; j++) {
-            const name = rd.string()
-            const type = read_type()
-            props.push({ name, type })
-        }
-
-        types.set(name, {name, fields, props})
-    }
-
-    return types
 }
